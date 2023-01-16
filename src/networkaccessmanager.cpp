bacd9f8c0c8db6b97eea94205ca364384363008d
Sadeq Albana <sadeq.albana@snono-systems.com>
2023-01-16 23:00:32 +0300

    working on documentation

--- src/networkaccessmanager.cpp
+++ src/networkaccessmanager.cpp
@@ -6,6 +6,12 @@ NetworkAccessManager::NetworkAccessManager(QObject *parent)
 
 }
 
+NetworkAccessManager *NetworkAccessManager::get(const QNetworkRequest &request)
+{
+    get(request);
+    return this;
+}
+
 void NetworkAccessManager::abortAllRequests()
 {
     for(QNetworkReply *reply : m_replies){
@@ -14,12 +20,23 @@ void NetworkAccessManager::abortAllRequests()
     m_replies.clear();
 }
 
+QNetworkRequest NetworkAccessManager::createNetworkRequest(const QUrl &url)
+{
+
+}
+
 
 QNetworkReply *NetworkAccessManager::createRequest(Operation op, const QNetworkRequest &originalReq, QIODevice *outgoingData)
 {
     QNetworkReply *reply=QNetworkAccessManager::createRequest(op,originalReq,outgoingData);
 
     m_replies << reply;
+    if(originalReq.attribute(static_cast<QNetworkRequest::Attribute>(RequstAttribute::MonitorProgressAttribute)).toBool()){
+        connect(reply,&QNetworkReply::downloadProgress,this,[this,originalReq](qint64 bytesReceived, qint64 bytesTotal){
+            int requestId=originalReq.attribute(static_cast<QNetworkRequest::Attribute>(RequstAttribute::MonitorProgressAttribute)).toInt();
+            emit this->downloadProgress(bytesReceived,bytesTotal,requestId);
+        });
+    }
     emit networkActivity(originalReq.url());
 
     return reply;
--- src/networkaccessmanager.h
+++ src/networkaccessmanager.h
@@ -8,6 +8,12 @@ class NetworkAccessManager : public QNetworkAccessManager
 {
     Q_OBJECT
 public:
+
+    enum RequstAttribute{
+      IdAttribute               = 1001, //a unique identifier for each request
+      MonitorProgressAttribute =  1002  //if set to true, the download \a progress() signal is emitted each time a chunck of data is received
+    };
+
     /*!
         Constructs a NetworkAccessManager object that is the center of
         the Network Access API and sets \a parent as the parent object.
@@ -15,14 +21,36 @@ public:
     explicit NetworkAccessManager(QObject *parent = nullptr);
 
 
+
+    /*!
+        sends a request with the passed \a url, internally creates a \a QNetworkRequset Object from the factory
+    */
+    NetworkAccessManager *get(const QUrl &url);
+
+    /*!
+        sends a request with the passed \a url, internally creates a \a QNetworkRequset Object from the factory
+
+    */
+    NetworkAccessManager *get(const QNetworkRequest &request);
+
     /*!
         aborts all requests that are being processed
         calls  \a QNetworkReply::abort() and deletes the \a NetworkRespnse * pointer that was created with \a createRequest()
     */
     void abortAllRequests();
 
+    QNetworkRequest createNetworkRequest(const QUrl &url);
+
 signals:
+    /*!
+        this signal is emitted after the start of each network request
+    */
     void networkActivity(QUrl url);
+    void downloadProgress(qint64 bytesReceived, qint64 bytesTotal, int requestId);
+
+    /*!
+        this signal is emitted after the end of each network request, the request's response may not be completed !
+    */
     void finishedNetworkActivity(QUrl url);
 
 
@@ -36,6 +64,7 @@ protected:
 
 private:
     QList<QNetworkReply *> m_replies;
+
 };
 
 #endif // NETWORKACCESSMANAGER_H
