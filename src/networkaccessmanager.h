f74daf3de58b649c78511d9f5c6d7744cccd1e06
Sadeq Albana <sadeq.albana@snono-systems.com>
2023-01-16 15:58:42 +0300

    working on doxygen documentation

--- src/networkaccessmanager.h
+++ src/networkaccessmanager.h
@@ -3,11 +3,22 @@
 
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
+class NetworkResponse *res;
 class NetworkAccessManager : public QNetworkAccessManager
 {
     Q_OBJECT
 public:
+    /*!
+        Constructs a NetworkAccessManager object that is the center of
+        the Network Access API and sets \a parent as the parent object.
+    */
     explicit NetworkAccessManager(QObject *parent = nullptr);
+
+
+    /*!
+        aborts all requests that are being processed
+        calls  \a QNetworkReply::abort() and deletes the \a NetworkRespnse * pointer that was created with \a createRequest()
+    */
     void abortAllRequests();
 
 signals:
@@ -16,7 +27,12 @@ signals:
 
 
 protected:
+    /*!
+        calls \a QNetworkAccessManager::createRequest internally
+
+    */
     QNetworkReply *createRequest(QNetworkAccessManager::Operation op, const QNetworkRequest &originalReq, QIODevice *outgoingData = nullptr);
+
 
 private:
     QList<QNetworkReply *> m_replies;
